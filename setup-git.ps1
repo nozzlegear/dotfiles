23b9b0be309d29d60a08e63267f8ee2d159e7b3c
Joshua Harms <joshua@nozzlegear.com>
2018-02-04 12:27:42 -0600

    Turn of git ignorecase; configure crlf for windows

--- setup-git.ps1
+++ setup-git.ps1
@@ -1,3 +1,6 @@
 git config --global --add alias.lol "log --graph --decorate --pretty=oneline --abbrev-commit --all"
 git config --global user.email "joshua@nozzlegear.com"
 git config --global user.name "Joshua Harms"
+git config --global core.ignorecase false  
+# Configure line endings for windows 
+git config --global core.autocrlf true
