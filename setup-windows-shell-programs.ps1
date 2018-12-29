6b9d3d086fdf04e5b90be7c60c84420494ea43ae
Joshua Harms <joshua@nozzlegear.com>
2018-12-28 17:02:40 -0800

    Replace boxstarter script with chocolatey install scripts

---
+++ setup-windows-shell-programs.ps1
@@ -0,0 +1,23 @@
+#!/bin/bash
+#requires -RunAsAdministrator
+
+choco install poshgit -y
+choco install vim -y
+choco install openssh -y
+choco install gow -y
+choco install nodejs -y
+choco install yarn -y
+choco install dotnetcore-sdk -y
+choco install nuget.commandline -y
+choco install paket.powershell -y
+choco install jq -y
+choco install fake -y
+choco install ripgrep -y
+choco install dart-sdk -y
+choco install powershell-core -y
+choco install du -y
+choco install sudo -y
+choco install azure-cli -y
+choco install curl -y
+choco install docker -y
+choco install docker-compose -y
