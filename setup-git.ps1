aae330591b263f056fc9cfdcf418d16174bf8360
Joshua Harms <joshua@nozzlegear.com>
2019-12-12 15:01:36 -0600

    Make script executable

--- setup-git.ps1
+++ setup-git.ps1
@@ -1,3 +1,5 @@
+#! /usr/bin/env pwsh
+
 git config --global --add alias.lol "log --graph --decorate --pretty=oneline --abbrev-commit --all"
 git config --global --add alias.last "log --decorate --pretty=oneline --abbrev-commit -3"
 git config --global user.email "joshua@nozzlegear.com"
