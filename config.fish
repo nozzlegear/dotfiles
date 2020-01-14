aa1db1071bd89e2047f17a58ef7abb8f05529b2b
Joshua Harms <joshua@nozzlegear.com>
2020-01-14 13:10:13 -0600

    Add function for transferring files with transfer.sh

--- config.fish
+++ config.fish
@@ -23,6 +23,31 @@ else
 	set -x r ~/repos
 end
 
+# Function for uploading/sharing files from the command line
+function transfer --description 'Upload a file to transfer.sh'
+    if [ $argv[1] ]
+        # set -l fileName $argv[1]
+        set -l uploadUrl
+
+        if test "$argv[1]" = "-"
+            set uploadUrl "https://transfer.sh/piped-file"
+        else 
+            set uploadUrl https://transfer.sh/(basename $argv[1])
+        end
+
+        # write to output to tmpfile because of progress bar
+        set -l tmpfile ( mktemp -t transferXXXXXX )
+        # Use the Max-Days header to expire the link after 3 days
+        curl --progress-bar -H "Max-Days: 3" --upload-file "$argv[1]" $uploadUrl >> $tmpfile
+        #curl --progress-bar -H "Max-Days: 3" --upload-file "$argv[1]" https://transfer.sh/(basename $argv[1]) >> $tmpfile
+        cat $tmpfile
+        command rm -f $tmpfile
+    else
+        echo 'usage: transfer FILE_TO_TRANSFER'
+    end
+end
+funcsave transfer
+
 # Set path to include things like dotnet tools
 # https://github.com/fish-shell/fish-shell/issues/527
 set -gx PATH ~/.dotnet/tools $PATH
