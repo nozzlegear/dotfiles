61d7060ac6f14883cc1dd534236ed578f2f1024c
Joshua Harms <joshua@nozzlegear.com>
2020-07-31 11:06:05 -0500

    Update fish config

--- config.fish
+++ config.fish
@@ -13,55 +13,66 @@ else
     set WSL_running false
 end
 
+# Install fish package manager if it doesn't exist
+# https://github.com/jorgebucaran/fisher
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    set fisherUrl "https://raw.githubusercontent.com/jorgebucaran/fisher/c142f61e51db3e456ce71180fd541bcfd328fb7b/fisher.fish"
+    curl "$fisherUrl" --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+    # Add fish-getopts for script argument parsing
+    fisher add jorgebucaran/fish-getopts
+end
+
 # Custom aliases and functions
 alias s 'git status'
 
 if test "$WSL_running" = true
-    # This assumes that wsl.conf has been edited to make WSL mount at /c instead of /mnt/c
-	set -x r '/c/Users/nozzlegear/repos'
+	set -x r '/mnt/c/Users/nozzlegear/repos'
 else 
 	set -x r ~/repos
 end
 
-# Function for uploading/sharing files from the command line
-function transfer --description 'Upload a file to transfer.sh'
-    if [ $argv[1] ]
-        # set -l fileName $argv[1]
-        set -l uploadUrl
-
-        if test "$argv[1]" = "-"
-            set uploadUrl "https://transfer.sh/piped-file"
-        else 
-            set uploadUrl https://transfer.sh/(basename $argv[1])
-        end
-
-        # write to output to tmpfile because of progress bar
-        set -l tmpfile ( mktemp -t transferXXXXXX )
-        # Use the Max-Days header to expire the link after 3 days
-        curl --progress-bar -H "Max-Days: 3" --upload-file "$argv[1]" $uploadUrl >> $tmpfile
-        #curl --progress-bar -H "Max-Days: 3" --upload-file "$argv[1]" https://transfer.sh/(basename $argv[1]) >> $tmpfile
-        cat $tmpfile
-        command rm -f $tmpfile
-    else
-        echo 'usage: transfer FILE_TO_TRANSFER'
-    end
-end
-funcsave transfer
-
 # Set path to include things like dotnet tools
 # https://github.com/fish-shell/fish-shell/issues/527
-set -gx PATH ~/.dotnet/tools $PATH
+set -gx PATH ~/.dotnet/tools ~/.local/bin $PATH
 
 # Docker port stuff
 if test "$WSL_running" = true
     set -gx DOCKER_HOST 'tcp://localhost:2375'
 end
 
+# env variables
+set -gx ASPNETCORE_ENVIRONMENT "Development"
+# Point CurseBreaker to the wow retail directory
+# https://github.com/AcidWeb/CurseBreaker
+set -gx CURSEBREAKER_PATH "/home/nozzlegear/Games/battlenet/drive_c/Program Files (x86)/World of Warcraft/_retail_"
+
 # Set preferred text editor
 set -gx EDITOR nvim
 
 # Fix gpg stuff
 set -gx GPG_TTY (tty)
+
+# Put this function to your .bashrc file.
+# Usage: mv oldfilename
+# If you call mv without the second parameter it will prompt you to edit the filename on command line.
+# Original mv is called when it's called with more than one argument.
+# It's useful when you want to change just a few letters in a long name.
+#
+# Also see:
+# - imv from renameutils
+# - Ctrl-W Ctrl-Y Ctrl-Y (cut last word, paste, paste)
+
+#function mv() {
+#  if [ "$#" -ne 1 ] || [ ! -f "$1" ]; then
+#    command mv "$@"
+#    return
+#  fi
+#
+#  read -ei "$1" newfilename
+#  command mv -v -- "$1" "$newfilename"
+#}
 
 # Use starship prompt
 # https://starship.rs
