c553921bc9e0966f940c597328751af696a8ec84
Joshua Harms <joshua@nozzlegear.com>
2020-08-10 16:41:17 -0500

    Add script for restoring the generic couchy database

---
+++ restore-couchy.fish
@@ -0,0 +1,31 @@
+#! /usr/bin/env fish 
+
+set scriptName (status -f)
+set password $argv[1]
+
+echo ""
+
+function printHelpAndExit -a err
+    set_color yellow;
+    echo "$err"
+    echo "Usage: $scriptName [couchy password]"
+    set_color normal;
+    exit 1
+end
+
+if test -n "$password"
+    set password $password
+else if test -n "$COUCHY_PASSWORD"
+    set password $COUCHY_PASSWORD
+else
+    printHelpAndExit "Missing couchy admin password. Pass via CLI or \$COUCHY_PASSWORD environment variable (set -x COUCHY_PASSWORD [value])."
+end
+
+set port 5984
+set username nozzlegear
+set volumeLocation "$HOME/docker/custom-volumes/couchy/data"
+
+echo "Restoring Couchy database with apache/couchdb:3 on port $port. Using '$username' as admin username, and '$volumeLocation' as the data volume location."
+echo ""
+
+docker run --restart "unless-stopped" --name "couchy" --volume "$volumeLocation:/opt/couchdb/data" -d -e "COUCHDB_USER=$username" -e "COUCHDB_PASSWORD=$password" -p "$port:5984" apache/couchdb
