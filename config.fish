8a789bd1462c4f086b3ad2c951c50e2643d1d01f
Joshua Harms <joshua@nozzlegear.com>
2021-05-20 10:36:14 -0500

    Update path

--- config.fish
+++ config.fish
@@ -63,7 +63,7 @@ end
 
 # Set path to include things like dotnet tools
 # https://github.com/fish-shell/fish-shell/issues/527
-set -gx PATH ~/.dotnet/tools ~/.local/bin ~/.yarn/bin $PATH
+set -gx PATH ~/.dotnet ~/.dotnet/tools ~/.local/bin ~/.yarn/bin $PATH
 
 # Docker port stuff
 if test "$WSL_running" = true
