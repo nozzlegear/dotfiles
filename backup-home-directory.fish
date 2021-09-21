3083a4d0217a9d7b559bc643eec9e9eb6927c5b6
Joshua Harms <joshua@nozzlegear.com>
2021-09-21 11:02:14 -0500

    Add script for home directory backup

---
+++ backup-home-directory.fish
@@ -0,0 +1,33 @@
+#! /usr/bin/env fish
+
+set fileName "$USER-backup-"(date --iso-8601=minutes)".tar.gz"
+
+sudo tar -zcpv \
+	--exclude "node_modules" \
+	--exclude "packages" \
+	--exclude "paket-files" \
+	--exclude ".rustup" \
+	--exclude ".local/share/Nuget/Cache" \
+	--exclude "*irtualBox VMs" \
+	--exclude "lutris" \
+	--exclude "Games" \
+	--exclude "Steam" \
+    --exclude ".steam" \
+	--exclude "NuGet/v3-cache" \
+	--exclude ".vscode-server" \
+	--exclude ".mozilla" \
+	--exclude ".cache" \
+    --exclude ".config/discord/Cache" \
+    --exclude ".npm/*cache" \
+    --exclude "Trash" \
+    --exclude "repos/AUR" \
+    --exclude "repos/**/*/bin" \
+    --exclude "repos/**/*/obj" \
+    --exclude "snap" \
+    -f "/tmp/$fileName" \
+    ~/
+
+set_color green
+echo ""
+echo "Home directory backed up to /tmp/$fileName"
+set_color normal
