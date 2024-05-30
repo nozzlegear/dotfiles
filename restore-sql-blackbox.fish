12343bb77c5106f7239e3285dc3b791a00e6c4e1
Joshua Harms <joshua@nozzlegear.com>
2024-05-29 23:21:00 -0500

    Add linux/amd64 platform flag with warning when running on arm64

--- restore-sql-blackbox.fish
+++ restore-sql-blackbox.fish
@@ -25,15 +25,35 @@ set port 1433
 set containerName "blackbox"
 set volume "blackbox"
 set imageName "mcr.microsoft.com/mssql/server:2022-latest"
+set platform "linux/amd64"
+set user_platform (uname -m)
 
 set_color green
 echo "Restoring SQL database as $containerName with image '$imageName'. Using '$volume' as the data volume."
 echo ""
 set_color normal
 
+# Note that functions in fish return exit codes, not boolean true/false
+if test "$user_platform" = "arm64"
+    set_color yellow
+    echo -n "[Warning]: "
+    set_color normal
+    echo -n "Image does not support your machine's arch type "
+    set_color yellow
+    echo -n "$user_platform"
+    set_color normal
+    echo -n ". Defaulting to platform "
+    set_color yellow
+    echo -n "$platform"
+    set_color normal
+    echo ". No user action is required. Rosetta 2 emulation layer will be used, which may incur a small performance impact on the container."
+    echo ""
+end
+
 podman run \
     -dit \
     --name "$containerName" \
+    --platform "$platform" \
     -v "$volume:/var/opt/mssql" \
     -e 'ACCEPT_EULA=Y' \
     -e "MSSQL_SA_PASSWORD=$password" \
