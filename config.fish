fed0ce8ddefb5d98b6f96def77b1a4be67ab0943
Joshua Harms <joshua@nozzlegear.com>
2024-08-12 10:44:27 -0500

    Remove obsolete functions
    These were moved to dedicated fish function files in the fish-functions folder

--- config.fish
+++ config.fish
@@ -61,44 +61,6 @@ else
     abbr i sudo apt install -y
 end
 
-function tag -a "tagValue" -d "Runs `git tag -s` on the current directory"
-    if test -z "$tagValue"
-        set_color yellow
-        echo "No tag value specified. Usage: `tag 1.2.3`"
-        set_color normal
-        return 1
-    end
-    git tag -s "$tagValue" -m "$tagValue"
-end
-
-function ctag -a "tagValue" -d "Commits and tags the current directory"
-    if test -z "$tagValue"
-        set_color yellow
-        echo "No tag value specified. Usage: `ctag 1.2.3`"
-        set_color normal
-        return 1
-    end
-
-    # Ensure there are files staged for commit
-    # Source: https://stackoverflow.com/a/3162492
-    set -l count (git diff --cached --numstat | wc -l)
-
-    if test "$count" -lt 1
-        set_color yellow
-        echo "No files are staged for commit. Either stage files to commit, or use the `tag` function to skip committing and only tag the last commit."
-        set_color normal
-        return 1
-    end
-
-    git commit -m "$tagValue";
-    and tag "$tagValue";
-end
-
-# A function to check the expiration date of an SSL certificate
-function ssl_expiry -a "domain" -d "Gets the SSL certificate expiration date for the given website"
-    echo | openssl s_client -connect "$domain:443" 2>/dev/null | openssl x509 -noout -enddate
-end
-
 if test "$WSL_running" = true
     set -x u '/mnt/c/Users/nozzlegear'
 	set -x r ~/repos
