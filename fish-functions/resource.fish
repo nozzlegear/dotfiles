9be9ad8bd46100d92037b456f7b08108090301e1
Joshua Harms <joshua@nozzlegear.com>
2025-12-21 17:10:45 -0600

    Update resource function

--- fish-functions/resource.fish
+++ fish-functions/resource.fish
@@ -1,3 +1,6 @@
 function resource
-    source ~/.config/fish/config.fish; and echo "Done!"
+    source ~/.config/fish/config.fish
+    and source ~/.config/fish/env.fish
+    and echo "Done!"
+    or echo "Failed!"
 end
