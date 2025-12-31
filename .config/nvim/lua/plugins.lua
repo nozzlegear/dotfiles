281e7f8b6a9d5aadfd478031afa762fea24fb389
Joshua Harms <joshua@nozzlegear.com>
2025-12-31 13:54:56 -0600

    Refactor Pkl nvim config with Apple's latest recommended setup

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -111,8 +111,35 @@ require("lazy").setup({
           })
       end
     },
-    { 'nvim-treesitter/nvim-treesitter' },
-    { 'apple/pkl-neovim',
-      dependencies = { "nvim-treesitter/nvim-treesitter" },
-    },
+    {
+      "apple/pkl-neovim",
+      lazy = true,
+      ft = "pkl",
+      dependencies = {
+        {
+          "nvim-treesitter/nvim-treesitter",
+          build = function(_)
+            vim.cmd("TSUpdate")
+          end,
+        },
+        "L3MON4D3/LuaSnip",
+      },
+      build = function()
+        require('pkl-neovim').init()
+
+        -- Set up syntax highlighting.
+        vim.cmd("TSInstall pkl")
+      end,
+      config = function()
+        -- Set up snippets.
+        require("luasnip.loaders.from_snipmate").lazy_load()
+
+        -- Configure pkl-lsp
+        vim.g.pkl_neovim = {
+          -- if pkl-lsp is installed with brew:
+          start_command = { "pkl-lsp" },
+          pkl_cli_path = "/opt/homebrew/bin/pkl"
+        }
+      end,
+    }
 })
