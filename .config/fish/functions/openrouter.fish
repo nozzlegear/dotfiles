57d1a472484af6094a5fc29b84cfcffc7abd621c
Joshua Harms <joshua@nozzlegear.com>
2026-08-12 15:11:50 -0500

    Write openrouter key to stdout when inside a command substitution
    This makes it possible to do things like `set foo (openrouter)` or
    `curl -H "Authorization: $(openrouter)"`, which couldn't be done before.
    
    type: enhancement
    scope: fish

--- .config/fish/functions/openrouter.fish
+++ .config/fish/functions/openrouter.fish
@@ -4,7 +4,15 @@ function openrouter --description 'Adds $OPENROUTER_API_KEY to the shell process
         # These variables may already be set by the parent shell, so check if they
         # need to be unlocked first
         if not set -q OPENROUTER_API_KEY
-            set -x OPENROUTER_TOKEN (security find-generic-password -a "openrouter" -s "Openrouter API key" -w)
+            set -x OPENROUTER_API_KEY (security find-generic-password -a "openrouter" -s "Openrouter API key" -w)
+        end
+
+        # Only echo if this is command substitution (e.g. `set foo (openrouter)` or `curl -H "Authorization: $(openrouter)"`)
+        if status is-command-substitution
+            echo "$OPENROUTER_API_KEY"
+        else
+            set_color yellow
+            echo 'Refusing to write key unless command substitution is used, e.g. `set foo (openrouter)` or `curl -H "Authorization: $(openrouter)"`'
         end
     end
 end
